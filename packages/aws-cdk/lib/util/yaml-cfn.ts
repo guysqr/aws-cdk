465802203518b450f1f8927e806a6c6e148cee35
Guy Morton <52842774+guysqr@users.noreply.github.com>
2022-07-22 16:51:22 +1000

    Removed yaml/types import and use
    Causes failure when running in Electron.

--- packages/aws-cdk/lib/util/yaml-cfn.ts
+++ packages/aws-cdk/lib/util/yaml-cfn.ts
@@ -1,6 +1,5 @@
 import * as yaml from 'yaml';
 import * as yaml_cst from 'yaml/parse-cst';
-import * as yaml_types from 'yaml/types';
 
 /**
  * Serializes the given data structure into valid YAML.
@@ -9,13 +8,7 @@ import * as yaml_types from 'yaml/types';
  * @returns a string containing the YAML representation of {@param obj}
  */
 export function serialize(obj: any): string {
-  const oldFold = yaml_types.strOptions.fold.lineWidth;
-  try {
-    yaml_types.strOptions.fold.lineWidth = 0;
-    return yaml.stringify(obj, { schema: 'yaml-1.1' });
-  } finally {
-    yaml_types.strOptions.fold.lineWidth = oldFold;
-  }
+  return yaml.stringify(obj, { schema: 'yaml-1.1' });
 }
 
 /**
